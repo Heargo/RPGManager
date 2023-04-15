43d55ae41f071afc068d528c088022ef3ea1bfdc
Hugo REY <hrey222@gmail.com>
2023-04-15 22:21:06 +0100

    nav bar synced

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthentificationService } from 'src/app/services/auth.services';
+import { GamesService } from 'src/app/services/games.services';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,11 +8,17 @@ import { AuthentificationService } from 'src/app/services/auth.services';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
-  isMJ = false;
-  isInGame = false;
   //isConnected = false;
   //use AuthentificationService to get the current user
-  constructor(private authService:AuthentificationService) {  }
+  constructor(private authService:AuthentificationService,private games:GamesService) {  }
+
+  isInGame():boolean{
+    return this.games.currentGame != null;
+  }
+
+  isMJ():boolean{
+    return this.games.IsUserHost();
+  }
 
   isConnected():boolean{
     return this.authService.isConnected;
