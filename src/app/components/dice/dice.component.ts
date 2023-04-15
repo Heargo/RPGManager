502d0be04e3831f0d12c18e61a477c324626f6c7
Hugo REY <hrey222@gmail.com>
2023-04-15 22:15:09 +0100

    dice component

--- src/app/components/dice/dice.component.ts
+++ src/app/components/dice/dice.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class DiceComponent {
 
+  diceNumber:number = 1;
+  diceFaces:number = 6;
+  rolls:number[] = [];
+
+  constructor() { }
+
+  getSum():number
+  {
+    return this.rolls.reduce((partialSum, a) => partialSum + a, 0);
+  }
+
+  getSize(n:number):string{
+    return Math.floor(Math.log10(n)) + 1 +'ch';
+  }
+
+  async RollAnimation()
+  {
+    //set the value to a random number and wait for .2s before changing it again
+    for(let i=0; i < 10; i++){
+      this.rolls = [Math.floor(Math.random() * this.diceFaces) + 1];
+      await new Promise(resolve => setTimeout(resolve, 50));
+    }
+  }
+
+  async onRollClick(){
+    await this.RollAnimation();
+    this.rolls = [];
+    for(let i = 0; i < this.diceNumber; i++){
+      this.rolls.push(Math.floor(Math.random() * this.diceFaces) + 1);
+    }
+  }
+
 }
